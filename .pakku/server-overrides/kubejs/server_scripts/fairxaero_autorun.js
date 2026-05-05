e7408836e524bfedab09e138f9e98676093969ea
Cobwebblox <69752942+Cobwebblox@users.noreply.github.com>
2026-05-05 20:10:29 +0200

    change fairxaero_autorun.js kubejs server
    Added fairxaero nether fix

--- .pakku/server-overrides/kubejs/server_scripts/fairxaero_autorun.js
+++ .pakku/server-overrides/kubejs/server_scripts/fairxaero_autorun.js
@@ -2,10 +2,18 @@ ServerEvents.loaded(event => {
     const server = event.server
 
     server.scheduleInTicks(400, () => {
-        server.runCommand('function xaero:autorun/set/silent/fairxaero')
+        server.runCommandSilent('function xaero:autorun/set/silent/fairxaero')
     })
 
     server.scheduleInTicks(600, () => {
-        server.runCommand('function xaero:silent/fairxaero')
+        server.runCommandSilent('function xaero:silent/fairxaero')
+    })
+
+    server.scheduleInTicks(440, () => {
+        server.runCommandSilent('function xaero:autorun/set/silent/xaerowmnetherisfair')
+    })
+
+    server.scheduleInTicks(640, () => {
+        server.runCommandSilent('function xaero:silent/xaerowmnetherisfair')
     })
 })
