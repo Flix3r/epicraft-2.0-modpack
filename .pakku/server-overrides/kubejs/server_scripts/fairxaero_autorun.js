0cbdb27ecf64fe4bd51f0525daa48816bc8fc35a
Cobwebblox <69752942+Cobwebblox@users.noreply.github.com>
2026-05-09 17:52:39 +0200

    fix fairxaero_autorun.js kubejs server

--- .pakku/server-overrides/kubejs/server_scripts/fairxaero_autorun.js
+++ .pakku/server-overrides/kubejs/server_scripts/fairxaero_autorun.js
@@ -1,19 +1,19 @@
 ServerEvents.loaded(event => {
     const server = event.server
 
-    server.scheduleInTicks(400, () => {
+    server.scheduleInTicks(600, () => {
         server.runCommandSilent('function xaero:autorun/set/silent/fairxaero')
     })
-
-    server.scheduleInTicks(600, () => {
-        server.runCommandSilent('function xaero:silent/fairxaero')
-    })
-
-    server.scheduleInTicks(440, () => {
+    
+    server.scheduleInTicks(800, () => {
         server.runCommandSilent('function xaero:autorun/set/silent/xaerowmnetherisfair')
     })
 
-    server.scheduleInTicks(640, () => {
+    server.scheduleInTicks(900, () => {
+        server.runCommandSilent('function xaero:silent/fairxaero')
+    })
+
+    server.scheduleInTicks(1000, () => {
         server.runCommandSilent('function xaero:silent/xaerowmnetherisfair')
     })
 })
